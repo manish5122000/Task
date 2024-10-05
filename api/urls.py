a2330538acaabb773742e8154ac06bb3a3d4012f
manish5122000 <mauryamanish5122000@gmail.com>
2024-10-05 21:37:38 +0530

    commit

---
+++ api/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from .views import *
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task API",
+        default_version='v1',
+        description="Task management API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+
+# API's url
+    path('tasks/', CreateTaskView.as_view(), name='create_task'),
+    # Change method here for PUT and DELETE
+    path('tasks/<int:task_id>/', TaskDetailView.as_view(), name='task_detail'),
+    path('all/tasks/', TaskListView.as_view(), name='task_list'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+# Frontend url
+    path('', home , name="home"),
+    path('tasks/create/', create_task, name='task_create'),   
+    path('tasks/list/', all_task_list, name='task_list_page'),
+    path('task/<int:task_id>/', task_by_id, name='task'), 
+    path('tasks/update/<int:task_id>/', update_task, name='update_task'),
+    path('tasks/delete/<int:task_id>/', delete_task, name='delete_task'),  
+
+  
+
+]
---
+++ api/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from rest_framework.views import APIView
+from django.contrib import messages  
+
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from drf_yasg.utils import swagger_auto_schema
+from .serializers import TaskSerializer
+
+# Create Task API
+class CreateTaskView(APIView):
+    @swagger_auto_schema(request_body=TaskSerializer)
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":201,"message": "Task Created Successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Task API's
+class TaskDetailView(APIView):
+    def get_object(self, task_id):
+        try:
+            return TaskModel.objects.get(id=task_id)
+        except TaskModel.DoesNotExist:
+            return None
+
+# Get Task API by Id
+    def get(self, request, task_id):
+        task = self.get_object(task_id)
+        if task is None:
+            return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TaskSerializer(task)
+        return Response({"status":200, "message": "Successfully","data":serializer.data})
+
+# Update Task API
+    @swagger_auto_schema(request_body=TaskSerializer)
+    def put(self, request, task_id):
+        task = self.get_object(task_id)
+        if task is None:
+            return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":200, "message": "Task Updated Successfully","data":serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# Delete Task API
+    def delete(self, request, task_id):
+        task = self.get_object(task_id)
+        if task is None:
+            return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+        task.delete()
+        return Response({"status":200, "message": "Task Deleted Successfully"})
+
+# Get All Task API
+class TaskListView(APIView):
+    def get(self, request):
+        tasks = TaskModel.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response({"status":200, "message": "Successfully","data":serializer.data})
+
+
+# Home Page
+def home(request):
+    return render(request,"index.html")
+
+# Create Task via Frontend
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        
+        task = TaskModel(title=title, description=description)
+        task.save()
+        messages.success(request, "Task created successfully!")
+        return redirect('task_create')  
+
+    return render(request, "create_task.html")
+
+# All task list via frontend
+def all_task_list(request):
+    tasks = TaskModel.objects.all()  
+    return render(request, "task_list.html", {'tasks': tasks}) 
+
+# Get task by Id via Frontend
+def task_by_id(request, task_id):
+    task = get_object_or_404(TaskModel, id=task_id)  
+    return render(request, "task.html", {'task': task})
+
+# Update task via frontend
+def update_task(request, task_id):
+    task = get_object_or_404(TaskModel, id=task_id) 
+
+    if request.method == 'POST':
+        task.title = request.POST.get('title') 
+        task.content = request.POST.get('description')  
+        task.status = request.POST.get('status') 
+        task.save()  
+        messages.success(request, "Task updated successfully!")  
+        return redirect('task_list_page')  
+
+    return render(request, "update_task.html", {'task': task})  
+
+# Delete Task via frontend
+def delete_task(request, task_id):
+    task = get_object_or_404(TaskModel, id=task_id)  
+    task.delete() 
+    messages.success(request, 'Task deleted successfully!') 
+    return redirect('task_list_page')
+
